473e55336b5221095233bea2f915b1dc68a0e687
sora <sora@mindlinksw.com>
2020-04-02 10:39:51 +0900

    re

--- react-nodebird/front/sagas/index.js
+++ react-nodebird/front/sagas/index.js
@@ -1,7 +1,7 @@
 import { all, call } from "redux-saga/effects";
 import user from "./user";
 import post from "./post";
-
+console.log('re');
 export default function* rootSaga() {
   yield all([call(user), call(post)]);
 }
